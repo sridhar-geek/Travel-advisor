16a28f303d744351553f06c15d4b6c44e983c8ac
Sridhar <sridharmoturu@gmail.com>
2023-06-30 12:45:57 +0530

    first commit

---
+++ src/Data/Data.js
@@ -0,0 +1,32 @@
+const images=[
+    {
+        id:1,
+        url:"https://source.unsplash.com/1300x700/?random" 
+    },
+    {
+        id:2,
+        url:"https://source.unsplash.com/1300x700/?random(1)" 
+    },
+    {
+        id:3,
+        url:"https://source.unsplash.com/1300x700/?random(2)" 
+    },
+    {
+        id:4,
+        url:"https://source.unsplash.com/1300x700/?random(3)" 
+    },
+    {
+        id:5,
+        url:"https://source.unsplash.com/1300x700/?random(4)" 
+    },
+    {
+        id:6,
+        url:"https://source.unsplash.com/1300x700/?random(5)" 
+    },
+    {
+        id:7,
+        url:"https://source.unsplash.com/1300x700/?random(6)" 
+    },
+]
+
+export default images;
